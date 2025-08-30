f6c2529b6d7aa1ab2611eb2acdc9c94e1d687efe
Angad Kochar <angadkochar2000@gmail.com>
2025-08-30 16:01:29 -0700

    Delete components directory

--- components/Logo.tsx
+++
@@ -1,8 +0,0 @@
-export default function Logo() {
-  return (
-    <div className="flex items-center gap-2 font-semibold">
-      <span className="inline-flex h-8 w-8 items-center justify-center rounded-xl bg-mint text-navy">↗</span>
-      <span>Fundora AI</span>
-    </div>
-  )
-}
