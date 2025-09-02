c7007c996e8182c9642d11e91c70d9ca827522de
Angad Kochar <angadkochar2000@gmail.com>
2025-09-01 23:00:31 -0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,1 +1,14 @@
-module.exports = { reactStrictMode: true };
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/grants',
+        destination: `${process.env.BACKEND_URL}/api/grants`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
