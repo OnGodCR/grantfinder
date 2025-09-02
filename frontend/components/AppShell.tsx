b1ffc581e0d38e9e3ae3db6dbd68918e365e6172
Angad Kochar <angadkochar2000@gmail.com>
2025-09-01 17:18:09 -0700

    Update AppShell.tsx

--- frontend/components/AppShell.tsx
+++ frontend/components/AppShell.tsx
@@ -4,7 +4,7 @@ import Sidebar from '@/components/Sidebar'
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-[100dvh]">
+    <div className="min-h-[100dvh] bg-navy flex">
       <Sidebar />
       <div className="flex-1 px-4 py-6 md:px-8 md:py-10">{children}</div>
     </div>
